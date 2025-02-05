7b239d89bcf5252c4a50a3f5166da250f9ae8f2c
tankankita <tankankita02@gmail.com>
2025-02-05 01:05:43 -0600

    check

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-import Portfolio from './Portfolio2.vue'
+// import Portfolio from './Portfolio2.vue'
+import Sid from './components/Sid.vue'
 import router from './router';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(Portfolio),
+  render: h => h(Sid),
 }).$mount('#app')
